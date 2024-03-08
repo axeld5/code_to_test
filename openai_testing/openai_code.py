25519c748095651257e71efcbd2fc0a814aa48f6
axeld5 <axel.darmouni@student-cs.fr>
2024-03-08 01:51:44 +0100

    solved more unittest problems

--- openai_testing/openai_code.py
+++ openai_testing/openai_code.py
@@ -17,18 +17,23 @@ def explain_code(code, openai_api_key):
     )
     return runnable.invoke({"code": code})
 
-def generate_inputs(code, openai_api_key):
+def generate_inputs(code, code_explanation, openai_api_key):
     prompt = PromptTemplate.from_template(
             """
-            You are an artificial intelligence language model which goal is to generate example inputs to be used within a given code.
+            You are an artificial intelligence language model which goal is to generate a list of at least 5 example inputs to be used within a given code.
+            Through the exploitation of a code explanation, your goal is also to generate examples which are as diverse as possible, in order to tackle as many edge cases as can be done.
+            Therefore: vary the amount of inputs if that is a parameter, the values of the inputs... In order for the tests to cover as wide as possible.
 
             Code: def multiply(a:float, b:float) -> float: return a*b
-            Example Inputs: a=5.1, b=10.2
+            Code explanation: This code multiplies two floats.
+            Example Inputs: [(5.1, 10.2), (500.3, 4.0), (12.3, 4.29), (-10.0, 3), (31.2, 5.4)]
 
             Code: def create_df(address_list:list[str], num_list:list[int]) -> pd.DataFrame: return pd.DataFrame("address": address_list, "num": num_list)
-            Example Inputs: address_list=["rue de la fontaine"], num_list=[10]
+            Code explanation: This code creates a dataframe from a list of adresses and numbers related to those addresses.
+            Example Inputs: [(["rue de la fontaine"], [10]), ..., (["baker street", "sesame avenue"], [222, 300])]
 
             Code: {code}
+            Code explanation: {code_explanation}
             Example Inputs:"""
     )
     runnable = (
@@ -36,16 +41,15 @@ def generate_inputs(code, openai_api_key):
     | ChatOpenAI(openai_api_key=openai_api_key, temperature=0)
     | StrOutputParser()
     )
-    return runnable.invoke({"code": code})
+    return runnable.invoke({"code": code, "code_explanation": code_explanation})
 
-def generate_unittest(code, code_explanation, inputs, openai_api_key):
+def generate_unittest(code, code_explanation, openai_api_key):
     prompt = PromptTemplate.from_template(
             """
             You are an artificial intelligence language model which goal is to generate a unittest code from a piece of code, using the mentioned inputs. You can help yourself from the code explanation.
             Code: {code}
             Code explanation: {code_explanation}
-            Inputs: {inputs}
-            Only return the test class. Do not return anything else.
+            Only return the test class. Do not return anything else. Let's think step by step.
             Unittest Code:"""
     )
     runnable = (
@@ -53,4 +57,22 @@ def generate_unittest(code, code_explanation, inputs, openai_api_key):
     | ChatOpenAI(openai_api_key=openai_api_key, temperature=0)
     | StrOutputParser()
     )
-    return runnable.invoke({"code": code, "code_explanation": code_explanation, "inputs": inputs})
+    return runnable.invoke({"code": code, "code_explanation": code_explanation})
+
+def generate_unittest_advanced(code, code_explanation, inputs, outputs, openai_api_key):
+    prompt = PromptTemplate.from_template(
+            """
+            You are an artificial intelligence language model which goal is to generate a unittest code from a piece of code, using the mentioned inputs and their related outputs. You can help yourself from the code explanation.
+            Code: {code}
+            Code explanation: {code_explanation}
+            Input_list: {inputs}
+            Output_list: {outputs}
+            Only return the test class. Do not return anything else. Let's think step by step.
+            Unittest Code:"""
+    )
+    runnable = (
+    prompt
+    | ChatOpenAI(openai_api_key=openai_api_key, temperature=0)
+    | StrOutputParser()
+    )
+    return runnable.invoke({"code": code, "code_explanation": code_explanation, "inputs": inputs, "outputs": outputs})
